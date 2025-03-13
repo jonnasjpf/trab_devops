a2aa7cb2ec980a2a820e6edd9d52f04ebb8de8c6
jonnasjpf <jonnas.flor@pucpr.edu.br>
2025-03-12 21:40:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import random
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -8,4 +10,4 @@ async def root():
 
 @app.get("/teste1")
 async def funcaoteste():
-    return {"teste": "deu certo"}
+    return {"teste": True, "num_aleatório": random.randint(0, 1000)}
